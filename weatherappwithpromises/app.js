ac75e121d6f34475a8b1e466d9479548fced8116
Raghusriram <sreeram.arghu01@gmail.com>
2019-02-20 16:43:30 +0530

    app added

--- weatherappwithpromises/app.js
+++ weatherappwithpromises/app.js
@@ -1,25 +1,6 @@
-// const request = require('request')
-//
-// request({
-//   url:'http://www.mapquestapi.com/geocoding/v1/address?key=dfApbuSb95GxYsXGcWO9PlKFV8rWCRsU&location=Bengaluru',
-//   json:true
-// },(error, response ,body) => {
-//   console.log(JSON.stringify(body, undefined, 2));
-//   console.log(body.results[0].locations[0].latLng.lat)
-//   console.log((body.results[0].locations[0].latLng.lng))
-// })
-
-
-
-
-/////////// ral http request ////////////
-
-
-// / address
-const yargs = require('yargs')
-
 const geocode = require('./geocode')
 const temp = require('./temperature')
+const yargs = require('yargs')
 const argv = yargs
   .options({
    a :{
